47672fa4585cc1b91f1a2b515b60f5a1aebc238e
NTTruc <trucnt@biso24.com>
2022-10-08 15:27:27 +0700

    Fix function find

--- src/service.ts
+++ src/service.ts
@@ -77,7 +77,7 @@ export abstract class BaseService<T> implements IBaseService<T> {
     return raw;
   }
 
-  async find(filter: Partial<T>, projection?: any, options?: any, callback?: any): Promise<T[]> {
+  async find(filter: Partial<T>, projection?: any, options?: any, callback?: any): Promise<T> {
     const raw = await this.repo.find(filter, projection, options, callback);
     return raw;
   }
