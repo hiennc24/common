93214667a068122d46113dffa2d43a4ec877f964
Hien Nguyen <hiennc@biso24.com>
2022-10-08 18:08:09 +0700

    Fixed bug

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -61,7 +61,7 @@ describe('CREATE User', () => {
     userService
       .create(user)
       .then((result) => {
-        expect(result).has.ownProperty('_id');
+        expect(result).has.ownProperty('id');
         expect(result.email).to.eqls(user.email);
         expect(result.name).to.eqls(user.name);
         done();
